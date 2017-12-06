0b08a8a527bd330b9afe112187302f2e3e2c4ead
tbenade <tbenade@seek.com.au>
2017-12-06 11:48:43 +1100

    Add nvm and flesh the response

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:6.11-alpine
+FROM node:9.2.0-alpine
 
 # Create app directory
 WORKDIR /usr/src/app
--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
 const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.get('/', (req, res) => {
+  console.log(req)
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  const response = {
+    message: 'Hello world!',
+    headers: req.headers,
+    method: req.method,
+    hostname: req.hostname,
+    ip: req.ip,
+    originalUrl: req.originalUrl
+  }
+  res.write(JSON.stringify(response, true, 2))
+  res.end()
+})
+
+app.listen(port, () => console.log(`Hello-world api listening on port ${port}!`))
